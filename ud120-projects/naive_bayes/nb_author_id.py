2db11082442af5c8bb879ff7dafdb5a6edd44e53
allisontngo <atngo.91@gmail.com>
2015-08-18 17:13:06 -0700

    added solution to naive bayes mini project

--- ud120-projects/naive_bayes/nb_author_id.py
+++ ud120-projects/naive_bayes/nb_author_id.py
@@ -15,7 +15,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -27,6 +27,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+def author_id(f_train, f_test, l_train, l_test):
+	clf = GaussianNB()
+	t0 = time()
+	clf.fit(f_train, l_train)
+	print "training time:", round(time()-t0, 3), "s"	
+	t0 = time()
+	pred = clf.predict(f_test)
+	print "predicting time:", round(time()-t0, 3), "s"
+	print 'accuracy  ',  clf.score(f_test, l_test)
+	return None
+
+
+author_id(features_train, features_test, \
+	labels_train, labels_test)
 
 
 #########################################################
