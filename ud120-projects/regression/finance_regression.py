500f26801545903431e27c0b72c513a0187514b8
allisontngo <atngo.91@gmail.com>
2015-08-19 19:08:42 -0700

    added solutions to linear regression lesson

--- ud120-projects/regression/finance_regression.py
+++ ud120-projects/regression/finance_regression.py
@@ -22,7 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "salary"] #'long_term_incentive'
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -30,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,8 +38,13 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color from "b" to "r"
 ### to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
-
+print 'coefficient is: ', reg.coef_[0]
+print 'intercept is: ', reg.intercept_
+print 'r-squared is: ', reg.score(feature_test, target_test)
 
 
 
@@ -65,6 +70,9 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+print 'slope of regression on test data: ', reg.coef_[0] 
+plt.plot(feature_train, reg.predict(feature_train), color="b")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
