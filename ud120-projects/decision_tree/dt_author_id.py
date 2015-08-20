3d8bdfbffb633f0d4482f30d120d85a3d0e93ce1
allisontngo <atngo.91@gmail.com>
2015-08-19 18:41:48 -0700

    added solutions to decisions tree lesson

--- ud120-projects/decision_tree/dt_author_id.py
+++ ud120-projects/decision_tree/dt_author_id.py
@@ -26,7 +26,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+def clf_pred(min_samp=40):
+	clf = tree.DecisionTreeClassifier(min_samples_split=min_samp)
+	clf.fit(features_train, labels_train)
+	return clf.predict(features_test)
 
+def accuracy(pred):
+	return accuracy_score(pred, labels_test)
+
+print 'features: ', len(features_train[0])
+print 'accuracy: %f' % accuracy(clf_pred())
 
 #########################################################
 
