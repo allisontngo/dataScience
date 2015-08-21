bd579ff622229c0565e5c66e7d3e25144dc3da54
allisontngo <atngo.91@gmail.com>
2015-08-20 19:55:25 -0700

    added solutions to kmeans mini project

--- ud120-projects/k_means/k_means_cluster.py
+++ ud120-projects/k_means/k_means_cluster.py
@@ -44,23 +44,33 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+#find min and max of feature
+def find_range(feature):
+	d = [data_dict[key][feature] for key in \
+ 		data_dict.keys() if data_dict[key][feature] != 'NaN']
+	return (min(d), max(d))
+
+print 'range of exercised_stock_options: ', find_range("exercised_stock_options")
+print 'range of salary: ', find_range('salary')
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
-data = featureFormat(data_dict, features_list )
+features_list = [poi, feature_1, feature_2, feature_3]
+data = featureFormat(data_dict, features_list)
 poi, finance_features = targetFeatureSplit( data )
+
 
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2, f3  in finance_features:
+    plt.scatter( f1, f2)
 plt.show()
 
 
@@ -76,6 +86,12 @@ Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=fe
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+features_list = ["poi", feature_1, feature_2, feature_3]
+data3 = featureFormat(data_dict, features_list)
+poi, finance_features = targetFeatureSplit(data3)
+clf = KMeans(n_clusters=2)
+pred = clf.fit_predict(finance_features)
+
 
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
